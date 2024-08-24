fe093516c6ad6f77bdc82298c5068d60c65741f6
Dung Huynh Duc <dunghd.it@gmail.com>
2024-08-24 11:27:53 +0800

    feat(install): add kitty terminal installation and update GNOME dependencies

--- install.sh
+++ install.sh
@@ -4,9 +4,13 @@
 echo "Updating the system..."
 sudo pacman -Syu --noconfirm
 
+# Install kitty
+echo "Installing kitty..."
+sudo pacman -S kitty --noconfirm
+
 # Install GNOME desktop environment
 echo "Installing GNOME desktop environment..."
-sudo pacman -S gnome --noconfirm
+sudo pacman -S xorg xorg-xinit gnome --noconfirm
 
 # Install GDM
 echo "Installing GDM..."
