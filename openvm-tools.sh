29fac7e105c8cfa11574611fffb5b4f84363941d
Dung Huynh Duc <dunghd.it@gmail.com>
2024-08-24 12:57:42 +0800

    fix: missing package on install openvm tools
    chore: missing package

--- openvm-tools.sh
+++ openvm-tools.sh
@@ -2,13 +2,13 @@
 
 # Install VMware Tools
 echo "Installing base-devel..."
-sudo pacman -S base-devel --noconfirm
+sudo pacman -S base-devel autoconf automake libtool xorg-server-devel glib2-devel pkg-config xmlsec --noconfirm
 
 echo "Cloning VMware Tools repository..."
-git clone https://github.com/daimaou92/install-arch-vmwarefusion-techpreview.git
+git clone https://github.com/daimaou92/install-arch-vmwarefusion-techpreview.git vmware-tools
 
 echo "Building and installing VMware Tools..."
-cd install-arch-vmwarefusion-techpreview/openvmtools
+cd vmware-tools/after/openvmtools
 sh build.sh
 
 # Reboot the system again to apply changes
