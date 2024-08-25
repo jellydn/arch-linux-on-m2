88594ccd59f8a7982df45fad16633ee35271b513
Dung Huynh Duc <dunghd.it@gmail.com>
2024-08-25 19:35:10 +0800

    feat: update script to install Open VMware Tools

--- openvm-tools.sh
+++ openvm-tools.sh
@@ -5,9 +5,11 @@ echo "Installing base-devel..."
 sudo pacman -S base-devel autoconf automake libtool xorg-server-devel glib2-devel pkg-config xmlsec --noconfirm
 
 echo "Cloning VMware Tools repository..."
+rm -rf vmware-tools
 git clone https://github.com/daimaou92/install-arch-vmwarefusion-techpreview.git vmware-tools
 
-echo "Building and installing VMware Tools..."
+echo "Building and installing Open VMware Tools..."
+rm -rf ~/open-vm-tools
 cd vmware-tools/after/openvmtools
 sh build.sh
 
